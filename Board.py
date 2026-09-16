17e86181d98b149491b84b9694eb1be44c1c8054
Phillip Brandel <pdbrandel@gmail.com>
2026-09-16 14:58:10 -0700

    added Board class

--- Board.py
+++ Board.py
@@ -1,8 +1,10 @@
 class Board:
-    def __init__(self,width,height):
-        self.data = [[0] * width for _ in range(height)]
-    
+    def __init__(self,width,depth):
+        self.data = [[0] * width for _ in range(depth)]
+        self.width = width
+        self.depth = depth
+
     def display(self):
         print("\033[2J\033[H",end="")
         for i in range(len(self.data)):
-            print(self.data[i])
+            print(*self.data[i])
